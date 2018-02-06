0d1d0a8b82fbfc00a56712e50498a18ada5d1f0c
Wikiki <wikiki@protonmail.com>
2018-02-06 07:49:40 +0100

    Polyfill Object.assign

--- datepicker.js
+++ datepicker.js
@@ -91,6 +91,36 @@ var datepicker_langs = {
   }
 }
 
+if (typeof Object.assign != 'function') {
+  // Must be writable: true, enumerable: false, configurable: true
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target, varArgs) { // .length of function is 2
+      'use strict';
+      if (target == null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var to = Object(target);
+
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) { // Skip over if undefined or null
+          for (var nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
+
 class DatePicker {
   constructor(selector, options = {}) {
     // Determine click event depending on if we are on Touch device or not
