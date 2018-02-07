daf3862663e31976f14ca7c9743b51d73f23e213
Wikiki <wikiki@protonmail.com>
2018-02-07 21:18:09 +0100

    Restructure files

--- src/datepicker.js
+++ src/datepicker.js
@@ -121,7 +121,7 @@ if (typeof Object.assign != 'function') {
   });
 }
 
-class DatePicker {
+export default class DatePicker {
   constructor(selector, options = {}) {
     // Determine click event depending on if we are on Touch device or not
     this._clickEvent = ('ontouchstart' in window) ? 'touchstart' : 'click';
