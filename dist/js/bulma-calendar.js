d8c67c77e6b42a5af8bdc1c6863f83f86c8f36d0
Adrien Rambert <adrien@rambert.me>
2019-10-04 11:38:01 +0200

    fix crypto usage for IE11

--- dist/js/bulma-calendar.js
+++ dist/js/bulma-calendar.js
@@ -9659,7 +9659,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 var uuid = function uuid() {
 	var prefix = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
 	return prefix + ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, function (c) {
-		return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16);
+		return (c ^ (window.crypto || window.msCrypto).getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16);
 	});
 };
 var deepMerge = function deepMerge() {
